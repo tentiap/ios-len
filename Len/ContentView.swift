1c4342df75415ac8342a95c33b79115e25acb780
tentiap <inooyabu@gmail.com>
2023-12-14 17:12:44 +0700

    Initial Commit

---
+++ Len/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Len
+//
+//  Created by Tenti Atika Putri on 14/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Len/LenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LenApp.swift
+//  Len
+//
+//  Created by Tenti Atika Putri on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
